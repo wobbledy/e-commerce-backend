9824f7c7dd801d65fbb70b599210ceb9cd8cc560
Zach Leone <zjleone@gmail.com>
2023-04-08 15:00:29 -0700

    added category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,16 +34,54 @@ router.get('/:id', async (req, res) => {
 
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// Creates a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// Updates a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// Deletes a category bt its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryData = await LibraryCard.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
